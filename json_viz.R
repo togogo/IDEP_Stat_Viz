261d151621c53b93e55713e8c4fe3de0c64decc8
togogo <togokida@gmail.com>
2020-04-18 16:52:35 +0900

    basic plot is achieved

--- json_viz.R
+++ json_viz.R
@@ -1,1 +1,13 @@
 #ok, let's write something...
+
+
+library(jsonlite)
+library(readr)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+x = fromJSON("./data/test_result.json") %>% as.data.frame
+
+testplot <- ggplot(x, aes(x = text.amount.count.elapsedSec, y = text.amount.count.textNum)) +geom_line()
+  
