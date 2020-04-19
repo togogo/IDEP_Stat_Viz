f34cdc324c693f331f8c0dfce3ed356bdb3e8852
togogo <togokida@gmail.com>
2020-04-19 23:41:31 +0900

    code revised as the json file is getting more complex.

--- json_viz.R
+++ json_viz.R
@@ -1,13 +1,18 @@
-#ok, let's write something...
-
-
+#libraries
 library(jsonlite)
 library(readr)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-x = fromJSON("./data/test_result.json") %>% as.data.frame
+#data reading
+x = fromJSON("./data/result_try2.json") #first read it as a list, then work on it individually.
 
-testplot <- ggplot(x, aes(x = text.amount.count.elapsedSec, y = text.amount.count.textNum)) +geom_line()
-  
+#data reorganizing
+textAmountCount <- data.frame(x['text amount count'])
+endText <- x['final text']
+genInfo <- data.frame(x['genaration info'])
+genText <- data.frame(x['generated text'])
+
+#plotting
+textAmountCountPlot <- ggplot(textAmountCount, aes(x = text.amount.count.elapsedSec, y = text.amount.count.textNum)) + geom_area()
