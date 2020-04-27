f95910c41581d7dc2388639c47e9e285332ae7e4
togogo <togokida@gmail.com>
2020-04-28 00:37:07 +0900

    I was able to connect separately recorded data as one session

--- json_viz.R
+++ json_viz.R
@@ -6,28 +6,35 @@ library(dplyr)
 library(ggplot2)
 
 #data reading
-#x = fromJSON("./data/result_try2.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try3.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try4.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try5.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try6.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try7.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try8.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try9.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try10.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try11.json") #first read it as a list, then work on it individually.
-#x = fromJSON("./data/result_try12.json") #first read it as a list, then work on it individually.
-x = fromJSON("./data/result_try13.json") #first read it as a list, then work on it individually.
 
+#TESTING
+#x = fromJSON("./data/preliminary_test/result_try15.json")#With AI, Subject 1
+
+#WITH AI
+#x = fromJSON("./data/group1_with_ai/with_ai_subject1_2020_04_25.json")#With AI, Subject 1 (Togo Kida)
+sub1 = fromJSON("./data/group1_with_ai/sakamoto/2020_4_27_18_3_45_result.json")#With AI, Subject 2-1 (Ai Sakamoto)
+sub2 = fromJSON("./data/group1_with_ai/sakamoto/2020_4_27_18_9_10_result.json")#With AI, Subject 2-2 (Ai Sakamoto)
+sub2AdjustedSec <- sub2$`text amount count`['elapsedSec'] + 551#adjust the elapsed sec count since it's coming after sub1
+sub2Adjusted <- data.frame(textNum = sub2$`text amount count`['textNum'], elapsedSec = sub2AdjustedSec)
+
+#NO AI
+x = fromJSON("./data/group3_no_help/no_help_subject1_2020_4_27_0_8_7_result.json")#No HelpI, Subject 1 (Yuka Kida)
+
+#databinding test
+#testbind <- rbind(sub1$`text amount count`, sub2$`text amount count`)
+testbind <- rbind(sub1$`text amount count`, sub2Adjusted)
 
 #data reorganizing
-textAmountCount <- data.frame(x['text amount count'])
+textAmountCount <- testbind
+#textAmountCount <- data.frame(sub1['text amount count'])
+#textAmountCount <- data.frame(sub2['text amount count'])
 endText <- x['final text']
 genInfo <- data.frame(x['generation info'])
 genText <- data.frame(x['generated text'])
 
 #plot making
-textAmountCountPlot <- ggplot(textAmountCount, aes(x = text.amount.count.elapsedSec, y = text.amount.count.textNum)) + geom_area()
+#textAmountCountPlot <- ggplot(textAmountCount, aes(x = text.amount.count.elapsedSec, y = text.amount.count.textNum)) + geom_line()
+textAmountCountPlot <- ggplot(textAmountCount, aes(x = elapsedSec, y = textNum)) + geom_line()
 
 #plotting
 textAmountCountPlot
